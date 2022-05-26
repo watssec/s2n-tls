02689342984857da425ec18b7d163b04ad483d0f
Ji Ru <rubujubi@gmail.com>
2022-05-26 11:04:25 -0700

    add constant value mutation

--- mutation/passes/Constant/Mutation/Mutation.cpp
+++ mutation/passes/Constant/Mutation/Mutation.cpp
@@ -29,37 +29,17 @@ namespace {
     SkeletonPass() : FunctionPass(ID) {}
 
     virtual bool runOnFunction(Function &F) {
-      for (auto &B : F) {
-        function_num = function_num + 1;
-        for (auto &I : B) {
-          instruction_num = instruction_num +1;
-          if (!(function_num == FunctionId and instruction_num == InstructionId)){
-            continue;
-          }
-          errs() << "Input file name " << InputFileName << "\n";
-
-          if (auto *op = dyn_cast<BinaryOperator>(&I)) {
-            // Insert at the point where the instruction `op` appears.
-            IRBuilder<> builder(op);
-
-            // Make a multiply with the same operands as `op`.
-            Value *lhs = op->getOperand(0);
-            Value *rhs = op->getOperand(1);
-            Value *mul = builder.CreateMul(lhs, rhs);
-
-            // Everywhere the old instruction was used as an operand, use our
-            // new multiply instruction instead.
-            for (auto &U : op->uses()) {
-              User *user = U.getUser();  // A User is anything with operands.
-              user->setOperand(U.getOperandNo(), mul);
+      for(auto B = F.getBasicBlockList().begin(); B != F.getBasicBlockList().end(); B++)
+        {
+            for(auto inst = B->begin(); inst != B->end(); inst++)
+            {
+                for(Use &U:inst->operands())
+                {
+                    Value *V = U.get();
+                    auto *op = dyn_cast<Constant>(V);
+                }
             }
-
-            // We modified the code.
-            
-          }
         }
-      }
-
       return true;  }
   };
 }
@@ -68,4 +48,4 @@ char SkeletonPass::ID = 0;
 
 // Automatically enable the pass.
 // http://adriansampson.net/blog/clangpass.html
-static RegisterPass<SkeletonPass> X("Binop", "Binary operator mutation", false, false);
+static RegisterPass<SkeletonPass> X("Constant", "Constant mutation", false, false);
