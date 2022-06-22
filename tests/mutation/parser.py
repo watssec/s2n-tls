ca9a92f9332a65e922da983ba83ae21e92fbc43c
r2ji <rubujubi@gmail.com>
2022-06-22 16:37:17 -0700

    .

--- tests/mutation/parser.py
+++ tests/mutation/parser.py
@@ -10,13 +10,11 @@ for saw_file_dir in glob.glob(root_path):
         print(saw_file_path)
         with open(saw_file_path,"r") as saw_file:
             file_content = saw_file.read()
-            pattern1 = r"m \"\w+\""
-            pattern2 = r"llvm \"\w+\""
+            pattern1 = r"crucible_llvm_verify \w+ \"\w+\""
+            pattern2 = r"crucible_llvm_unsafe_assume_spec \w+ \"\w+\""
             function_list1 = re.findall(pattern1, file_content)
             function_list2 = re.findall(pattern2, file_content)
-            print(function_list2)
-            test_saw_match_json[saw_file_path.split("/")[-1]] = function_list1 + function_list2 
-
+            test_saw_match_json[saw_file_path.split("/")[-1]] = function_list1+ function_list2
            
 
 json.dump(test_saw_match_json, test_saw_match_file,indent=4)
