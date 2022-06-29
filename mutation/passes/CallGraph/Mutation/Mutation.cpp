9789a1a46985cc4d1606e9776cf0c1ab764431e9
r2ji <rubujubi@gmail.com>
2022-06-28 21:15:44 -0700

    callgraph by DFS iterator

--- mutation/passes/CallGraph/Mutation/Mutation.cpp
+++ mutation/passes/CallGraph/Mutation/Mutation.cpp
@@ -12,13 +12,21 @@
 #include "llvm/IR/DebugInfoMetadata.h"
 #include "llvm/Analysis/CallGraph.h"
 #include "llvm/ADT/DepthFirstIterator.h"
+#include "nlohmann/json.hpp"
+#include <fstream>
+#include <vector>
+#include <iostream>
+#include "string"
+#include "cstring"
 
 using namespace llvm;
+using json = nlohmann::json;
 
 static cl::opt<std::string> InputFileName("file_name", cl::desc("Specify input filename"), cl::value_desc("filename"));
 static cl::opt<int> FunctionId("function_num", cl::desc("Specify function number"), cl::value_desc("function number"));
 static cl::opt<int> InstructionId("instruction_num", cl::desc("Specify instruction number"), cl::value_desc("instruction number"));
-
+// In a DFS, if the Pathlength of the current node is smaller or equal to the previous one, then it has reached a leaf node.
+// Once a leaf node is found, dump the path.
 
 namespace {
 
@@ -32,14 +40,50 @@ namespace {
     virtual bool runOnModule(Module &M) {
 
     CallGraph CG = CallGraph(M); 
-      
+    int cnt = 0;
+    int prev_node_length = 1;
+    auto prev_node = df_begin(&CG);
+    json path_array = json::array();
+    
     for (auto IT = df_begin(&CG), EI = df_end(&CG); IT != EI; IT++) {
-      dbgs() << "get Path Length" << IT.getPathLength() << "\n";
-      if (Function *F = IT->getFunction()) {
+  
+     
+      
+
+      
+      if (prev_node_length > IT.getPathLength()){
         
-        dbgs() << "Visiting function: " << F->getName() << "\n";
+        std::vector<std::string> path_node;  
+       
+        json single_path = json::object();
+        
+        for(int i = 0; i< prev_node_length; i++){
+          dbgs() << "prev" << prev_node.getPath(i)->getFunction() << "\n";
+          
+          if(Function *F = prev_node.getPath(i)->getFunction()){
+            path_node.push_back(F->getName());
+            dbgs() << "fun name " << F->getName() << "\n";
+          }else{
+            path_node.push_back("0x0");
+            dbgs() << "no name" << "0x0" << "\n";
+          }
+          
+        }
+        
+        single_path[std::to_string(cnt)] = path_node; 
+        path_array.push_back(single_path);
+        
       }
+      
+      prev_node_length = IT.getPathLength();
+      
+      prev_node++;
+      cnt = cnt +1;
     }
+
+    std::ofstream o ("call_graph_all.json", std::ofstream::trunc);
+    o << std::setw(4) << path_array << std::endl;
+  
     }
   };
 }
