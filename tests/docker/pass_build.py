fbe8996b2b6e26d46f702abf2300475c766a41fc
r2ji <r2ji@tick1.cs.uwaterloo.ca>
2022-08-22 00:54:47 +0000

    update docker support

--- tests/docker/pass_build.py
+++ tests/docker/pass_build.py
@@ -1,7 +1,20 @@
 import os
-pass_dir = "../../mutation/passes"
+import glob
+
+pass_root_dir = "/project/mutation/passes"
 
 #go into each pass and cmake .. make
+
+for pass_dir in glob.glob(pass_root_dir+"/**"):
+    pass_name = pass_dir.split("/")[-1]
+    build_dir = pass_root_dir+ "/"+pass_name+ "/build"
+    os.system("rm -rf "+ build_dir)
+    print("Building "+ pass_name +"...")
+    if not os.path.exists(build_dir):
+        os.system("mkdir "+ build_dir)
+    os.chdir(build_dir)
+    os.system("cmake ..")
+    os.system("make")
 
 
 
